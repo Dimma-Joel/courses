37068aa2e947f23fe976908d980e25a2a7164ae5
Dimma Joel <dimmajoel@gmail.com>
2025-05-08 02:01:59 +0100

    Initial commit

--- complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,1 +1,35 @@
-alert = "Hi Dimma";
+let js = "amazing";
+if (js === "amazing")
+  // alert("javascript is FUN");
+  console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+//Values and variables
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "Programmer";
+let job2 = "Teacher";
+
+//Values and variables practice
+let country = "Nigeria";
+let continent = "Africa";
+let population = 500;
+
+console.log(country);
+console.log(continent);
+console.log(population);
