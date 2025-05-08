1f8f5fdbfb709145482f78628f34de9d97d72b94
Dimma Joel <dimmajoel@gmail.com>
2025-05-08 03:20:56 +0100

    Data types practice

--- complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,31 +1,4 @@
-let js = "amazing";
-if (js === "amazing")
-  // alert("javascript is FUN");
-  console.log(40 + 8 + 23 - 10);
-
-console.log("Jonas");
-console.log(23);
-
-let firstName = "Matilda";
-
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
-
-//Values and variables
-let jonas_matilda = "JM";
-let $function = 27;
-
-let person = "jonas";
-let PI = 3.1415;
-
-let myFirstJob = "Programmer";
-let myCurrentJob = "Teacher";
-
-let job1 = "Programmer";
-let job2 = "Teacher";
-
-//Values and variables practice
+//////Values and variables practice
 let country = "Nigeria";
 let continent = "Africa";
 let population = 500;
@@ -33,3 +6,57 @@ let population = 500;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+//////Data types practice
+let isIsland = "false";
+let language;
+
+console.log(country);
+console.log(population);
+console.log(isIsland);
+console.log(language);
+
+/////////////////////////
+// Values and variables
+
+// let js = "amazing";
+// if (js === "amazing")
+//   // alert("javascript is FUN");
+//   console.log(40 + 8 + 23 - 10);
+
+// console.log("Jonas");
+// console.log(23);
+
+// let firstName = "Matilda";
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// let jonas_matilda = "JM";
+// let $function = 27;
+
+// let person = "jonas";
+// let PI = 3.1415;
+
+// let myFirstJob = "Programmer";
+// let myCurrentJob = "Teacher";
+
+// let job1 = "Programmer";
+// let job2 = "Teacher";
+
+//////////////////////////
+// Data Types
+// let javascriptIsFUN = true;
+// console.log(javascriptIsFUN);
+
+// console.log(typeof true);
+// console.log(typeof 23);
+// console.log(typeof "dimma");
+
+// javascriptIsFUN = "YES";
+// console.log(typeof javascriptIsFUN);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
